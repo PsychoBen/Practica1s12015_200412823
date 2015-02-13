f6a8afc1f73a8a9949b73f0adef11740036bba6f
Ben <Ben@Ben-PC>
2015-02-13 09:52:37 -0600

    12/02/2015

---
+++ src/misEstructuras/NodoPersonaje.java
@@ -0,0 +1,17 @@
+/**
+ * Universidad de San Carlos de Guatemala
+ * Facultad de Ingenieria
+ * Ingenieria en Ciencias y Sistemas
+ * Esdras Benjamin Cotto Revolorio
+ * Carnet: 200412823
+ */
+
+package misEstructuras;
+
+/**
+ *
+ * @author Ben
+ */
+public class NodoPersonaje {
+    
+}
