3ccb9dfc94fbd2e250104b444345fd9850675af5
Ben <Ben@Ben-PC>
2015-02-12 19:18:06 -0600

    12/02/2015

---
+++ src/misEstructuras/NodoCampoTexto.java
@@ -0,0 +1,18 @@
+/**
+ * Universidad de San Carlos de Guatemala
+ * Facultad de Ingenieria
+ * Ingenieria en Ciencias y Sistemas
+ * Esdras Benjamin Cotto Revolorio
+ * Carnet: 200412823
+ */
+package misEstructuras;
+
+/**
+ *
+ * @author Ben
+ */
+public class NodoCampoTexto {
+    private String texto;
+    
+    
+}
